e1dc58c2b62b7144f0589f33cfd1247a1dcb0d4d
junep16 <jiyunpark0816@gmail.com>
2022-03-11 22:52:44 +0900

    ✅ Row 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Navigation from './components/Navigation';
 import Banner from "./components/Banner"; 
+import Row from './components/Row';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Banner />
+      <Row 
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchURL={Request.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" id="TN" fetchURL={Request.fetchTrending}/>
+      <Row title="Top Rated" id="TR" fetchURL={Request.fetchTopRated}/>
+      <Row title="Action Movies" id="AM" fetchURL={Request.fetchActionMovies}/>
+      <Row title="Comedy Movies" id="CM" fetchURL={Request.fetchComedyMovies}/>
     </div>
   );
 }
