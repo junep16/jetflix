76a7a688c6011dab9c4cb817b3331c2953aa50fb
junep16 <jiyunpark0816@gmail.com>
2022-03-13 14:30:07 +0900

    🩹 app.js Row 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ function App() {
       <Row title="Top Rated" id="TR" fetchURL={requests.fetchTopRated}/>
       <Row title="Action Movies" id="AM" fetchURL={requests.fetchActionMovies}/>
       <Row title="Comedy Movies" id="CM" fetchURL={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" id="HM" fetchURL={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" id="RM" fetchURL={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" id="DM" fetchURL={requests.fetchDocumentaries}/>
+
     </div>
   );
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -21,7 +21,11 @@ export default function Row({ title, fetchURL, isLargeRow, id }) {
       <h2>{title}</h2>
       <div className='slider'>
         <div className='slider__arrow-left'>
-          <span className='arrow'>{'<'}</span>  
+          <span className='arrow' 
+          onClick={() => {
+            document.getElementById(id).scrollLeft -= window.innerWidth -80; 
+          }}
+          >{'<'}</span>  
         </div>
       <div id={id} className="row__posters">
         { movies.length > 0 && movies.map((movie) => (
@@ -34,10 +38,14 @@ export default function Row({ title, fetchURL, isLargeRow, id }) {
         ))}
       </div>
         <div className='slider__arrow-right'>
-          <span className='arrow'>{'>'}</span>  
+          <span className='arrow'
+          onClick={() => {
+            document.getElementById(id).scrollLeft += window.innerWidth -80;
+          }}
+          
+          >{'>'}</span>  
         </div>
       </div>
-
     </section>
   )
 }
