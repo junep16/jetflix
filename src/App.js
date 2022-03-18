43f569fbaadfa3b2d3ae0f0da7d6763f850efb07
junep16 <jiyunpark0816@gmail.com>
2022-03-18 22:47:42 +0900

    ✅ detail page 추가

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
-import React from "react"; 
-import { Outlet, Route, Routes } from "react-router-dom"; 
-import './App.css';
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
-import MainPage from "./pages/MainPage"; 
-import DetailPage from './pages/DetailPage';
-import SearchPage from './pages/SearchPage';
+import { Outlet, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Nav from "./components/Navigation";
+import DetailPage from "./pages/DetailPage";
+import MainPage from "./pages/MainPage";
+import SearchPage from "./pages/SearchPage";
 
 const Layout = () => {
   return (
     <div>
-      <Navigation />
+      <Nav />
 
       <Outlet />
 
       <Footer />
     </div>
-
-
-  )
-
-}
-
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Layout />}>
+    <div className="app">
+      <Routes> 
+        <Route path="/" element={<Layout />} >
           <Route index element={<MainPage />} />
-          <Route path =":movieId" element={<DetailPage />} />
-          <Route path ="search" element={<SearchPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
         </Route>
       </Routes>
     </div>
   );
 }
+
 export default App;
--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,66 +1,72 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { useLocation } from "react-router-dom";  
-import { useDebounce } from '../../hooks/useDebounce';
-import "./SearchPage.css"; 
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "../../api/axios";
+import { useDebounce } from "../../hooks/useDebounce";
+import "./SearchPage.css";
 
 export default function SearchPage() {
-  const [searchResults, setSearchResults] = useState([]); 
-
+  const navigate = useNavigate();
+  const [searchResults, setSearchResults] = useState([]);
   const useQuery = () => {
-    return new URLSearchParams(useLocation().search); 
-  }
+    return new URLSearchParams(useLocation().search);
+  };
 
-  let query = useQuery(); 
-  const searchTerm = query.get("q"); 
-  const debouncedSearchTerm = useDebounce(searchTerm, 500); 
-  console.log(searchTerm); 
-
+  let query = useQuery();
+  const searchTerm = query.get("q");
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
   useEffect(() => {
-    if(debouncedSearchTerm) {
-      fetchSearchMovie(debouncedSearchTerm);  
+    if (debouncedSearchTerm) {
+      fetchSearchMovie(debouncedSearchTerm);
     }
-  }, [debouncedSearchTerm]); 
+  }, [debouncedSearchTerm]);
 
-  const fetchSearchMovie = async () => {
+  const fetchSearchMovie = async (searchTerm) => {
+    console.log("searchTerm", searchTerm);
     try {
-      const request = await axios.get(`/search/multi?include_adult=false&query=${searchTerm}`)
-      console.log(request); 
-      setSearchResults(request.data.results); 
+      const request = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      console.log(request);
+      setSearchResults(request.data.results);
     } catch (error) {
-      console.log("error", error); 
+      console.log("error", error);
     }
-  }
+  };
 
   const renderSearchResults = () => {
     return searchResults.length > 0 ? (
-      <section className ='search-container'>
-      {searchResults.map((movie) => {
-        if (movie.backdrop_path !== null && movie.media_type !== "person"){
-          const movieImageUrl = 
-            "https://image.tmdb.org/t/p/w500" + movie.backdrop_path 
-          return (
-            <div className='movie' key={movie.id}>
-              <div className='movie__column-poster'>
-                <img 
-                  src={movieImageUrl} 
-                  alt="movie-image" 
-                  className='movie__poster'></img>  
-              </div>  
-            </div>
-          )
-        }
-      })}
+      <section className="search-container">
+        {searchResults.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== "person") {
+            const movieImageUrl =
+              "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+            return (
+              <div className="movie" key={movie.id}>
+                <div
+                  onClick={() => navigate(`/${movie.id}`)}
+                  className="movie__column-poster"
+                >
+                  <img
+                    src={movieImageUrl}
+                    alt="movie"
+                    className="movie__poster"
+                  />
+                </div>
+              </div>
+            );
+          }
+        })}
       </section>
     ) : (
-        <section className='no-results'>
-          <div className='no-results__text'>
-            <p>
-              찾고자 하는 검색어 "{debouncedSearchTerm}"에 맞는 영화가 없습니다. 
-            </p>
-          </div>
-        </section> 
-    )
-  }
-  return renderSearchResults(); 
+      <section className="no-results">
+        <div className="no-results__text">
+          <p>
+            찾고자하는 검색어"{debouncedSearchTerm}"에 맞는 영화가 없습니다.
+          </p>
+        </div>
+      </section>
+    );
+  };
+
+  return renderSearchResults();
 }
